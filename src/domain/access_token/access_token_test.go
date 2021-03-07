f12c370d4fd4fa37e5a8fd3d77f6bb263c0689f2
Atnlie <anton@hellonimbly.com>
2021-03-07 19:10:26 +0700

    feat(token): token and unit test

---
+++ src/domain/access_token/access_token_test.go
@@ -0,0 +1,32 @@
+package access_token
+
+import (
+	"testing"
+	"time"
+)
+
+func TestGetNewAccessToken(t *testing.T) {
+	at := GetNewAccessToken()
+	if at.IsExpired() {
+		t.Error("brand new access token should not be expired")
+	}
+
+	if at.AccessToken != "" {
+		t.Error("new access token should not have defined access token id")
+	}
+
+	if at.UserId != 0 {
+		t.Error("new access token should not have an associated user id")
+	}
+}
+
+func TestAccessToken_IsExpired(t *testing.T) {
+	at := AccessToken{}
+	if !at.IsExpired(){
+		t.Error("default access token should be expired by default")
+	}
+	at.Expires = time.Now().UTC().Add(3 * time.Hour).Unix()
+	if at.IsExpired() {
+		t.Error("error bro")
+	}
+}

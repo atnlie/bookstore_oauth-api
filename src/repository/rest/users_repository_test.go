c4c508a4005e40442bb1e4a1b10a8c704f09cc38
Atnlie <anton@hellonimbly.com>
2021-03-14 18:02:55 +0700

    fix(test): coverage test 100 percent

--- src/repository/rest/users_repository_test.go
+++ src/repository/rest/users_repository_test.go
@@ -56,9 +56,41 @@ func TestLoginUserInvalidErrorInterface(t *testing.T) {
 }
 
 func TestLoginUserInvalidUserJsonResponse(t *testing.T) {
+	rest.FlushMockups()
+	rest.AddMockups(&rest.Mock{
+		URL:          "http://localhost:8080/user/login",
+		HTTPMethod:   http.MethodPost,
+		ReqBody:      `{"email":"email@gmail.com","password":"the-password"}`,
+		RespHTTPCode: http.StatusOK,
+		RespBody:     `{"id":"1","first_name":"atnlie@gmail.com","last_name":"kurniyanto"}`,
+	})
 
+	repository := usersRepository{}
+
+	user, err := repository.LoginUser("email@gmail.com", "the-password")
+
+	assert.Nil(t, user, nil)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
+	assert.EqualValues(t, "error when trying unmarshal users response", err.Message)
 }
 
 func TestLoginUserNoError(t *testing.T) {
+	rest.FlushMockups()
+	rest.AddMockups(&rest.Mock{
+		URL:          "http://localhost:8080/user/login",
+		HTTPMethod:   http.MethodPost,
+		ReqBody:      `{"email":"email@gmail.com","password":"the-password"}`,
+		RespHTTPCode: http.StatusNotFound,
+		RespBody:     `{"message":"invalid login credentials","status":"404","error":"not_found"}`,
+	})
 
+	repository := usersRepository{}
+
+	user, err := repository.LoginUser("email@gmail.com", "the-password")
+
+	assert.Nil(t, user, nil)
+	assert.NotNil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, err.Status)
+	assert.EqualValues(t, "invalid interface error when trying to login user", err.Message)
 }
